f5f3433d4091fe64119e6c411b7cc0e6cadbfb88
devkhapra-tech <devkhapra0603@gmail.com>
2025-06-23 23:30:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from main import predict
+st.title('IPC PREDICTION SYSTEM')
+
+
+user_input = st.text_area("Describe the crime", placeholder="e.g., Theft in a house using a knife in Delhi")
+top_n = st.slider("How many IPC sections to show?", 1, 10, 5)
+
+if st.button("Predict"):
+    if user_input.strip():
+        results = predict(user_input, top_n)
+        st.success(f"Top {top_n} Relevant IPC Sections:")
+        for i, res in enumerate(results, 1):
+            st.markdown(f"**{i}. {res['section']}**  \nSimilarity Score: `{res['score']:.4f}`")
+    else:
+        st.warning("Please enter a crime description.")
---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+# Load IPC info
+ipc_df = pd.read_csv("ipc_sections.csv")  # Use the IPC Kaggle dataset
+
+# Clean
+ipc_df = ipc_df.dropna(subset=["Section", "Description"])
+ipc_df["Section_Text"] = "Section " + ipc_df["Section"].astype(str) + ": " + ipc_df["Description"]
+
+# Load BERT model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Generate embeddings
+ipc_embeddings = model.encode(ipc_df["Section_Text"].tolist(), convert_to_tensor=True)
+
+def predict(crime_text,top_k):
+    user_embedding = model.encode([crime_text], convert_to_tensor=True)
+    similar = cosine_similarity(user_embedding.cpu().numpy(), ipc_embeddings.cpu().numpy())[0]
+
+    top_indices=torch.topk(torch.tensor(similar),k=top_k,dim=0).indices
+    results = []
+    for idx in top_indices:
+        idx=int(idx)
+        results.append({
+            "section": ipc_df.iloc[idx]["Section_Text"],
+            "score": float(similar[idx])
+        })
+    return results
